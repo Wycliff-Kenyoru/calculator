cfd8a55f4086dd905ba5323f3097a7e5de83900d
Wycliff-Kenyoru <wycliffkerongo@gmail.com>
2018-07-04 10:00:10 +0300

    html added

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ var add = function(number1, number2) {
       var number2 = prompt("Enter another number:");
 
       alert(add(number1, number2));
-      
+
     var add = function(number1, number2) {
         return number1 + number2;
       };
@@ -24,4 +24,13 @@ var add = function(number1, number2) {
       var number1 = parseInt(prompt("Enter a number:"));
       var number2 = parseInt(prompt("Enter another number:"));
 
-      alert(add(number1, number2);
+      alert(add(number1, number2));
+
+
+// console.log('working')
+//
+// var list = [1,2,3,4,5,6,7]
+//
+// for (var i = 0; i < list.length; i++){
+//   console.log('current number is ' + list[i])
+// };
