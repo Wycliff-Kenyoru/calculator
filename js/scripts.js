4b9918bea8f326951eff95573ef056f220c7428b
Wycliff-Kenyoru <wycliffkerongo@gmail.com>
2018-07-04 11:27:42 +0300

    js fixed

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,7 @@ var add = function(number1, number2) {
       alert(add(number1, number2));
 
 
+
 // console.log('working')
 //
 // var list = [1,2,3,4,5,6,7]
@@ -34,3 +35,10 @@ var add = function(number1, number2) {
 // for (var i = 0; i < list.length; i++){
 //   console.log('current number is ' + list[i])
 // };
+
+//
+// var test = function (){
+//   prompt('Are you comming to class tomorrow')
+// };
+//
+// test();
